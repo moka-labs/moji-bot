cf2e0a7447b8410eb46871cd4959f4880d205e62
kamyu <kamyu1537@gmail.com>
2021-01-27 14:39:42 +0900

    기본 이모지일 경우 twemoji 를 대신 보내도록 변경

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
+const { parse } = require('twemoji-parser');
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -13,14 +15,22 @@ client.on('message', msg => {
   const regex = new RegExp("^<(a)?:(\\w+):(\\d{18})>$", "g")
   const match = regex.exec(msg.content);
 
+  let url = '';
   if (match) {
     const animated = match[1] !== undefined;
 
-    let url = `https://cdn.discordapp.com/emojis/${match[3]}.`;
+    url = `https://cdn.discordapp.com/emojis/${match[3]}.`;
     if (animated) url += "gif"
     else url += "png"
     url += "?v=1"
+  } else if (msg.content.length === 2) {
+    const entities = parse(msg.content, { assetType: 'png' });
+    if (entities.length === 1) {
+      url = entities[0].url;
+    }
+  }
 
+  if (url !== '') {
     const embed = new Discord.MessageEmbed();
     embed.setImage(url);
     embed.setAuthor(msg.author.tag, msg.author.displayAvatarURL());
