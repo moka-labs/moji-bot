7c88dd733b8bd483034eae0062cb55750aa2b1f7
kamyu <kamyu1537@gmail.com>
2021-01-27 15:34:48 +0900

    activity 추가, 멘션시 초대링크 보내는 기능 추가

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ const { parse } = require('twemoji-parser');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity({ name: 'big emotes' }).then();
 });
 
 client.on('message', msg => {
+  if (msg.guild == null) return;
   if (msg.author.bot) return;
   if (msg.author === client.user) return;
 
-  const regex = new RegExp("^<(a)?:(\\w+):(\\d{18})>$", "g")
+  if (msg.mentions.users.size === 1) {
+    const user = msg.mentions.users.values().next();
+    if (user.value === client.user) {
+      msg.delete().then();
+      msg.author.createDM()
+        .then(dm => {
+          const embed = new Discord.MessageEmbed();
+          embed.setTitle('Invite URL');
+          embed.setDescription(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=11264`);
+          embed.setColor('YELLOW');
+          dm.send(embed).then();
+        });
+    }
+
+    return;
+  }
+
+  const regex = new RegExp('^<(a)?:(\\w+):(\\d{18})>$', 'g');
   const match = regex.exec(msg.content);
 
   let url = '';
@@ -20,9 +38,9 @@ client.on('message', msg => {
     const animated = match[1] !== undefined;
 
     url = `https://cdn.discordapp.com/emojis/${match[3]}.`;
-    if (animated) url += "gif"
-    else url += "png"
-    url += "?v=1"
+    if (animated) url += 'gif';
+    else url += 'png';
+    url += '?v=1';
   } else {
     const entities = parse(msg.content, { assetType: 'png' });
     if (entities.length === 1) {
@@ -38,7 +56,7 @@ client.on('message', msg => {
     embed.setImage(url);
     embed.setAuthor(msg.author.tag, msg.author.displayAvatarURL());
 
-    // 임베드 색상
+    // Embed Color
     const member = msg.guild.member(msg.author);
     if (member !== null) {
       embed.setColor(msg.guild.member(msg.author).displayColor);
