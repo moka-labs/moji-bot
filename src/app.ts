678d7042af25d66ee02199a74b8299a078c9ef16
Byeon Seongun <kamyu1537@gmail.com>
2022-02-24 17:21:20 +0900

    split app

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import Eris from 'eris';
+import { readyHandler } from './handlers/ready';
+import { messageCreateHandler } from './handlers/message-create';
+import { interactionCreateHandler } from './handlers/interaction-create';
+import { commands } from './commands';
+
+export const app = async () => {
+  const client = new Eris.Client(process.env.BOT_TOKEN ?? '');
+
+  client.on('ready', () => readyHandler(client));
+  client.on('messageCreate', (msg) => messageCreateHandler(client, msg));
+  client.on('interactionCreate', (interaction) => interactionCreateHandler(client, interaction));
+
+  await client.connect();
+  return client;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import 'dotenv/config';
-import Eris from 'eris';
-import { readyHandler } from './handlers/ready';
-import { messageCreateHandler } from './handlers/message-create';
+import { app } from './app';
 
-const client = new Eris.Client(process.env.BOT_TOKEN ?? '');
-
-client.on('ready', () => readyHandler(client));
-client.on('messageCreate', (msg) => messageCreateHandler(client, msg));
-
-client.connect();
-process.on('SIGINT', function() {
-  client.disconnect({});
-  process.exit(0);
+app().then(client => {
+  process.on('SIGINT', function() {
+    client.disconnect({});
+    process.exit(0);
+  });
 });
